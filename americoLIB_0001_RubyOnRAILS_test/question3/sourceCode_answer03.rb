2bc6d8e145bc915d2d6c9676366f88a4c2bd3cc3
Américo Tomás <atafs@users.noreply.github.com>
2014-12-11 07:17:29 +0000

    Update sourceCode_answer03.rb

--- americoLIB_0001_RubyOnRAILS_test/question3/sourceCode_answer03.rb
+++ americoLIB_0001_RubyOnRAILS_test/question3/sourceCode_answer03.rb
@@ -1,20 +1,20 @@
 class MyController < ApplicationController 
   
-  def options 
+	def options 
     
-	#Ruby: arrays are created with [] brackets; can have nothing when initialized;
-	options = [] 
+		#Ruby: arrays are created with [] brackets; can have nothing when initialized;
+		options = [] 
     
-	available_option_keys = [:first_option, :second_option, :third_option] 		#another array
-    all_keys = params.keys.map(&:to_sym) 										#a third array with all the keys turned into symbols
-    set_option_keys = all_keys & available_option_keys 							#placing two arrays in the same one
-    set_option_keys.each do |key| 												#for each (loop) 
+		available_option_keys = [:first_option, :second_option, :third_option] 		#another array
+    		all_keys = params.keys.map(&:to_sym) 						#a third array with all the keys turned into symbols
+    		set_option_keys = all_keys & available_option_keys 				#placing two arrays in the same one
+    		set_option_keys.each do |key| 							#for each (loop) 
       
-	  #the keys are symbols, so it requires :
-	  options[key] = params[:key] 
-    end
+	 		#the keys are symbols, so it requires :
+	 		options[key] = params[:key] 
+   		end
 
-	#at the end we get an array with all the keys
-    options 
-  end 
+		#at the end we get an array with all the keys
+		options 
+  	end
 end 
