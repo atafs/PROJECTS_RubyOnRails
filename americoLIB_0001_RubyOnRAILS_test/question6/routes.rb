90ff4d7430ebbe152ff33beb9c5ef755fb817579
americotomasafs <americotomasafs@gmail.com>
2014-12-11 01:35:33 +0000

    load answers to TEST

---
+++ americoLIB_0001_RubyOnRAILS_test/question6/routes.rb
@@ -0,0 +1,22 @@
+#folder: config/locales/route.rb
+
+serverName::Application.routes.draw do
+
+	#URL path like /beer/
+	resources :beers do
+	
+		#URL path like /beer/<beer_type>/
+		resources :IPA
+		resources :brown_ale
+		resources :pilsner
+		resources :lager
+		resources :lambic
+		resources :hefeweizen
+		
+		#missing the code for error 404
+	
+	end	
+end
+	
+
+
